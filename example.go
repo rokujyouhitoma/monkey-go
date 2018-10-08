8ecd330bbeedf0bb7666d4f4aa2104d96af0e022
rokujyouhitoma <rokujyouhitomajp@gmail.com>
2018-10-09 00:07:49 +0900

    1.5. REPL

--- example.go
+++ example.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-    "errors"
+	"errors"
 )
 
-func example(code string) (int,error) {
-  if code == "hoge" {
-       return 1,nil
-  }
-  return 0,errors.New("code must be hoge")
+func example(code string) (int, error) {
+	if code == "hoge" {
+		return 1, nil
+	}
+	return 0, errors.New("code must be hoge")
 }
